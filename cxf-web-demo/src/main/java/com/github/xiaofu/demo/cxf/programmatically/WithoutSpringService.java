4a10b575342b94b53ccebea24f1bc893fd875f13
wfygowxf <wfygowxf1@163.com>
2015-02-15 15:35:43 +0800

    cxf web demo

--- cxf-web-demo/src/main/java/com/github/xiaofu/demo/cxf/programmatically/WithoutSpringService.java
+++ cxf-web-demo/src/main/java/com/github/xiaofu/demo/cxf/programmatically/WithoutSpringService.java
@@ -13,32 +13,41 @@ import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import com.github.xiaofu.demo.cxf.web.BooksService;
 import com.github.xiaofu.demo.cxf.web.CustomerService;
 import com.github.xiaofu.demo.cxf.web.PersonServiceImpl;
- 
+
 /**
  * 需要引入cxf-rt-transports-http-jetty
+ * 
  * @author xiaofu
  *
  */
 public class WithoutSpringService {
 	public static void main(String[] args) {
 		JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
-		//context props
-		 Map<String,Object> maps=new HashMap<String,Object>();
-		 maps.put("fiql.support.single.equals.operator", true);
-		 maps.put("search.use.all.query.component", true);
-		 sf.setProperties(maps);
-		sf.setResourceClasses(PersonServiceImpl.class,CustomerService.class,BooksService.class);
+		// disable this functionality
+		// context props
+	/*	Map<String, Object> maps = new HashMap<String, Object>();
+		maps.put("fiql.support.single.equals.operator", "true");
+		maps.put("search.use.all.query.component", "true");
+		sf.setProperties(maps);*/
+		
+		sf.setResourceClasses(PersonServiceImpl.class, CustomerService.class,
+				BooksService.class);
 		sf.setProvider(new SearchContextProvider());
 		sf.setProvider(new JacksonJsonProvider());
-		sf.setResourceProvider(PersonServiceImpl.class, new SingletonResourceProvider(new PersonServiceImpl()));
+		sf.setResourceProvider(PersonServiceImpl.class,
+				new SingletonResourceProvider(new PersonServiceImpl()));
 		sf.getInInterceptors().add(new LoggingInInterceptor());
 		sf.getOutInterceptors().add(new LoggingOutInterceptor());
 		sf.setAddress("http://localhost:9000/");
-		//官网上说加下面代码可以不用spring,但是我不加一样可以不用spring
-		/*BindingFactoryManager manager = sf.getBus().getExtension(BindingFactoryManager.class);
-		JAXRSBindingFactory factory = new JAXRSBindingFactory();
-		factory.setBus(sf.getBus());
-		manager.registerBindingFactory(JAXRSBindingFactory.JAXRS_BINDING_ID, factory);*/
+		// 官网上说加下面代码可以不用spring,但是我不加一样可以不用spring
+		/*
+		 * BindingFactoryManager manager =
+		 * sf.getBus().getExtension(BindingFactoryManager.class);
+		 * JAXRSBindingFactory factory = new JAXRSBindingFactory();
+		 * factory.setBus(sf.getBus());
+		 * manager.registerBindingFactory(JAXRSBindingFactory.JAXRS_BINDING_ID,
+		 * factory);
+		 */
 		sf.create();
 	}
 }
--- cxf-web-demo/src/main/java/com/github/xiaofu/demo/cxf/web/BooksService.java
+++ cxf-web-demo/src/main/java/com/github/xiaofu/demo/cxf/web/BooksService.java
@@ -1,5 +1,6 @@
 package com.github.xiaofu.demo.cxf.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,28 +10,46 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
+import org.apache.cxf.jaxrs.ext.search.SearchBean;
 import org.apache.cxf.jaxrs.ext.search.SearchCondition;
 import org.apache.cxf.jaxrs.ext.search.SearchContext;
+import org.apache.cxf.jaxrs.ext.search.sql.SQLPrinterVisitor;
 
 @Path("/books")
-@Produces({"text/xml","application/xml","application/json"}) 
+@Produces({ "text/xml", "application/xml", "application/json" })
 public class BooksService {
 
 	private static Map<Long, Book> books;
 	@Context
 	private SearchContext context;
 	static {
-		books=new HashMap<Long,Book>();
-		Book book1=new Book();
+		books = new HashMap<Long, Book>();
+		Book book1 = new Book();
 		book1.setId(1);
 		book1.setName("t1book");
-		Book book2=new Book();
+		Book book2 = new Book();
 		book2.setId(2);
 		book2.setName("t2book");
 		books.put(Long.valueOf(1), book1);
 		books.put(Long.valueOf(2), book2);
 	}
+
 	@GET
+	@Path("/sql")
+	public String getSql() {
+		SearchCondition<SearchBean> sc = context.getCondition(SearchBean.class);
+
+		Map<String, String> fieldMap = new HashMap<String, String>();
+		fieldMap.put("level", "LEVEL_COLUMN");
+		List<String> columns = new ArrayList<String>();
+		SQLPrinterVisitor<SearchBean> visitor = new SQLPrinterVisitor<SearchBean>(
+				fieldMap, "table", columns);
+		sc.accept(visitor);
+		return visitor.getQuery();
+	}
+
+	@GET
+	@Path("/book")
 	public List<Book> getBook() {
 		SearchCondition<Book> sc = context.getCondition(Book.class);
 		// SearchCondition#isMet method can also be used to build a list of
