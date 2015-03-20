5ca343452ce02eaf069cb2e810b88b7a4a06f48d
wfygowxf <wfygowxf1@163.com>
2015-03-20 16:08:36 +0800

    jdk demo

--- jdk-demo/src/main/java/com/github/xiaofu/demo/Test.java
+++ jdk-demo/src/main/java/com/github/xiaofu/demo/Test.java
@@ -7,66 +7,66 @@ import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
-public class Test
-{
-	public static void main(String[] args)
-	{
-	 System.out.println(MDCodingHour("www.cqvip.com"));
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(MDCodingHour("www.cqvip.com"));
+		System.out.println(newMDCodingHour("www.cqvip.com"));
 	}
 
-	public static String MDCodingHour(String tmp)
-	{
+	public static String newMDCodingHour(String tmp) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'a', 'b', 'c', 'd', 'e', 'f' };
+		String result = "";
+		try {
+			byte[] strTemp = tmp.getBytes();
+			MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+			mdTemp.update(strTemp);
+			byte[] md = mdTemp.digest();
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte byte0 = md[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			result = new String(str);
+		} catch (Exception e) {
+
+		}
+		return result;
+
+	}
+
+	public static String MDCodingHour(String tmp) {
 		int ii = 1;
 		Date datest = new Date();
 		long a = datest.getTime();
 		a /= 0x36ee80L;
 		String datestr = Long.toString(a);
-		System.out.println("=====datestr=====");
-		System.out.println(datestr);
+		
 		String datestr1 = datestr + tmp;
 		String jm = "";
-		char hexDigits[] = {
-				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
-				'c', 'd', 'e', 'f'
-		};
-		try
-		{
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'a', 'b', 'c', 'd', 'e', 'f' };
+		try {
 			byte strTemp[] = datestr1.getBytes();
-			
-			System.out.println("==datestr1 bytes==");
-			for (int i = 0; i < strTemp.length; i++)
-			{
-				StringBuilder builder=new StringBuilder("datestr1:");
-				builder.append(strTemp[i]+" ");
-				System.out.println(builder);
-			}
-			
+
 			MessageDigest mdTemp = MessageDigest.getInstance("MD5");
 			mdTemp.update(strTemp);
 			byte md[] = mdTemp.digest();
-			
-			System.out.println("==md bytes==");
-			for (int i = 0; i < md.length; i++)
-			{
-				StringBuilder builder=new StringBuilder("md:");
-				builder.append(md[i]+" ");
-				System.out.println(builder);
-			}
-			
+
 			int j = md.length;
 			char str[] = new char[j * 2];
 			int k = 0;
-			for (int i = 0; i < j; i++)
-			{
+			for (int i = 0; i < j; i++) {
 				byte byte0 = md[i];
 				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
 				str[k++] = hexDigits[byte0 & 0xf];
 			}
 
 			jm = new String(str);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.print("md5校验错误");
 			return "error";
 		}
