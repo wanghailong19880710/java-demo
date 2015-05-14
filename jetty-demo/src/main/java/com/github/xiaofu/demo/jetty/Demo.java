e3793fce473d614dd1881fe2408997e62e81abcf
wfygowxf <wfygowxf1@163.com>
2015-05-14 22:08:03 +0800

    jetty demo initial

---
+++ jetty-demo/src/main/java/com/github/xiaofu/demo/jetty/Demo.java
@@ -0,0 +1,20 @@
+package com.github.xiaofu.demo.jetty;
+/**
+ * 
+ */
+
+/**
+ * @author xiaofu
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
