1b406dc7472df05bc62fba0f427590d26451c54a
wfygowxf <wfygowxf1@163.com>
2015-05-25 17:30:48 +0800

    quartz demo

---
+++ quartz-demo/src/main/java/com/github/xiao/demo/quatz/Demo.java
@@ -0,0 +1,31 @@
+/**
+ * @ProjectName: quartz-demo
+ * @Copyright: 版权所有 Copyright © 2001-2012 cqvip.com Inc. All rights reserved. 
+ * @address: http://www.cqvip.com
+ * @date: 2015年5月25日 下午5:08:51
+ * @Description: 本内容仅限于维普公司内部使用，禁止转发.
+ */
+package com.github.xiao.demo.quatz;
+
+/**
+ * <p></p>
+ * @author flh 2015年5月25日 下午5:08:51
+ * @version V1.0   
+ * @modificationHistory=========================逻辑或功能性重大变更记录
+ * @modify by user: {修改人} 2015年5月25日
+ * @modify by reason:{方法名}:{原因}
+ */
+public class Demo
+{
+
+	/**
+	 * @author flh 2015年5月25日 下午5:08:51
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		 
+
+	}
+
+}
