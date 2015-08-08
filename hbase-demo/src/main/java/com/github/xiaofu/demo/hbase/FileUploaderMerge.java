24cb84c38fdcc23415535d43d97b4d399dedc4fc
wfygowxf <wfygowxf1@163.com>
2015-08-08 12:38:58 +0800

    hbase demo

--- hbase-demo/src/main/java/com/github/xiaofu/demo/hbase/FileUploaderMerge.java
+++ hbase-demo/src/main/java/com/github/xiaofu/demo/hbase/FileUploaderMerge.java
@@ -8,11 +8,17 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Writables;
 
 /**
  * @author fulaihua
@@ -54,10 +60,41 @@ public class FileUploaderMerge {
 			HMerge.merge(conf, FileSystem.get(fsConf),
 					Bytes.toBytes(TABLE),null,null);
 		} finally {
-			admin.enableTable(TABLE);
+			//admin.enableTable(TABLE);
 			admin.close();
 		}
+		scanTableRegionFromMeta();
 
+	}
+	public static void scanTableRegionFromMeta() throws IOException
+	{
+		Scan scan=new Scan();
+		//scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("regioninfo"));
+		scan.addFamily( HConstants.CATALOG_FAMILY);
+		scan.setStartRow(HRegionInfo.createRegionName(Bytes.toBytes(TABLE),
+				HConstants.EMPTY_START_ROW,
+				Bytes.toBytes(HConstants.ZEROES), false));
+		scan.setStopRow(HRegionInfo.createRegionName(Bytes.toBytes(TABLE),
+				Bytes.toBytes( HConstants.NINES),
+				Bytes.toBytes(HConstants.NINES), false));
+		HTable table=new HTable(conf, HConstants.META_TABLE_NAME);
+		ResultScanner scanner= table.getScanner(scan);
+		try {
+			
+			for (Result result : scanner) {
+			   System.out.println((HRegionInfo) Writables.getWritable(
+					  result.getValue(HConstants.CATALOG_FAMILY, HConstants.REGIONINFO_QUALIFIER), new HRegionInfo()));
+				//System.out.println(result.toString());
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		finally
+		{
+			scanner.close();
+			table.close();
+		}
 	}
 
 }
