04e5c5cfb0cb12a92b964b9d2a8eb43851af6fc4
wfygowxf <wfygowxf1@163.com>
2015-11-01 11:02:03 +0800

    cli test

---
+++ jdk-demo/src/main/java/com/github/xiaofu/demo/CliTest.java
@@ -0,0 +1,31 @@
+package com.github.xiaofu.demo;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class CliTest {
+	public static void main(String[] args) throws ParseException {
+		Options opts = new Options();
+		Option op = OptionBuilder.withArgName("configuration").hasArg()
+				.create("D");
+		Option op1 = OptionBuilder.withArgName("configuration").hasArg()
+				.create("conf");
+		opts.addOption(op);
+		opts.addOption(op1);
+		 CommandLineParser parser = new GnuParser();
+		 CommandLine cmd = parser.parse(opts, new String[]{"-Dconf=23","-Dtest=5","-conf=2.xml","-conf=3.xml"}, true);
+		 for (String item : cmd.getOptionValues("D")) {
+			System.out.println(item);
+		}
+		 for (String item : cmd.getOptionValues("conf")) {
+				System.out.println(item);
+			}
+		 
+
+	}
+}
