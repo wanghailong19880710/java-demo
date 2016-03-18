b07f6bbe59461ee4ee6286da7d42322c155d90b8
xiaofu <wfygowxf1@163.com>
2016-03-18 17:22:40 +0800

    jdk

--- jdk-demo/src/main/java/com/github/xiaofu/demo/InnerClassTest.java
+++ jdk-demo/src/main/java/com/github/xiaofu/demo/InnerClassTest.java
@@ -2,11 +2,14 @@ package com.github.xiaofu.demo;
 
 public class InnerClassTest {
 
+	
 	public ITest start(final int interval) {
 		
 		ITest test = new ITest() {
+			private int[] tt;
 			@Override
 			public void doIt() {
+				tt=new int[1024*1024*10];
 				System.out.println(interval);
 			}
 		};
@@ -14,7 +17,7 @@ public class InnerClassTest {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		InnerClassTest test = new InnerClassTest();
 		ITest[] tests=new ITest[10];
 		for (int i = 0; i < 10; i++) {
@@ -22,7 +25,9 @@ public class InnerClassTest {
 		}
 		for (ITest iTest : tests) {
 			iTest.doIt();	 
+		
 		}
+		Thread.currentThread().join();
 	}
 
 	interface ITest {
