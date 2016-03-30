ec002617f0feee7fda91918aa0e576f26f4a76ec
xiaofu <wfygowxf1@163.com>
2016-03-30 15:36:06 +0800

    jdk-demo/.classpath

--- jdk-demo/src/main/java/com/github/xiaofu/demo/InnerClassTest.java
+++ jdk-demo/src/main/java/com/github/xiaofu/demo/InnerClassTest.java
@@ -2,15 +2,17 @@ package com.github.xiaofu.demo;
 
 public class InnerClassTest {
 
-	
+	//5*4 字节
+	private static final int[]  outer=new int [1024*1024*5];
 	public ITest start(final int interval) {
-		
+	
 		ITest test = new ITest() {
 			private int[] tt;
 			@Override
 			public void doIt() {
 				tt=new int[1024*1024*10];
 				System.out.println(interval);
+				System.out.println(outer.length);
 			}
 		};
 		return test;
@@ -18,6 +20,7 @@ public class InnerClassTest {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
+	  System.out.println("new inner step1=============="+(Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory()));
 		InnerClassTest test = new InnerClassTest();
 		ITest[] tests=new ITest[10];
 		for (int i = 0; i < 10; i++) {
@@ -27,7 +30,7 @@ public class InnerClassTest {
 			tests[i].doIt();
 			tests[i]=null;//会被垃圾回收？它的回收机制又是什么？
 		}
-		 
+		 System.out.println("new inner after=============="+(Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory()));
 		Thread.currentThread().join();
 	}
 
